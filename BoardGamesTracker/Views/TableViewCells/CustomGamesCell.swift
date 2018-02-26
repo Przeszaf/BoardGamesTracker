d588c6603b299ab4be8a971ab590e09a65ceb02f
Przemyslaw Szafulski <p.szafulski@gmail.com>
2018-02-26 13:18:12 +0100

    Change file structure to more readable

---
+++ BoardGamesTracker/Views/TableViewCells/CustomGamesCell.swift
@@ -0,0 +1,47 @@
+//
+//  CustomGamesCell.swift
+//  BoardGamesTracker
+//
+//  Created by Przemyslaw Szafulski on 17/02/2018.
+//  Copyright © 2018 Przemyslaw Szafulski. All rights reserved.
+//
+
+import UIKit
+
+//Cell used to display all players
+class CustomGameCell: UITableViewCell {
+    
+    var gameNameLabel =  UILabel()
+    var gameTypeLabel = UILabel()
+    var gameIconImageView = UIImageView()
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        contentView.addSubview(gameNameLabel)
+        contentView.addSubview(gameTypeLabel)
+        contentView.addSubview(gameIconImageView)
+        
+        gameNameLabel.font = UIFont.systemFont(ofSize: 17)
+        gameTypeLabel.font = UIFont.systemFont(ofSize: 10)
+        gameTypeLabel.textColor = UIColor.darkGray
+        
+        gameIconImageView.layer.cornerRadius = 20
+        gameIconImageView.layer.masksToBounds = true
+    }
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        
+        gameNameLabel.frame = CGRect(x: 55, y: 10, width: frame.width - 60, height: 30)
+        gameTypeLabel.frame = CGRect(x: 55, y: 40, width: frame.width - 60, height: 15)
+        gameIconImageView.frame = CGRect(x: 5, y: 5, width: 40, height: 40)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+}
